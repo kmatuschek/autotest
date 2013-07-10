c9c869c63c304c9c4a1cf46e7fbfc89bfce7d123
Administrator <administrator@Administrators-MacBook-Pro-2.local>
2013-07-10 09:37:33 +0200

    added tests for Character

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Administrator on 10.07.13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
